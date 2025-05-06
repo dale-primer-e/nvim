52ebc33a3f24c7c06ccf46655e8520c34cf15c40
dale-primer-e <70494025+dale-primer-e@users.noreply.github.com>
2025-05-06 13:58:55 +1200

    Remove complicated parts - start basic config
    Working my way through a youtube video:
    https://www.youtube.com/watch?v=zHTeCSVAFNY&list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn

--- init.lua
+++ init.lua
@@ -1,7 +1,30 @@
--- Entry point for Neovim configuration
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 
--- Set leader key before lazy setup (this must come first)
-vim.g.mapleader = "'"
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
--- Load plugin configuration
-require("config.lazy")
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin-latte"
