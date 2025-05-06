6b39a51f75dd33727244dc1f225ccd21e0f01bd5
dale-primer-e <70494025+dale-primer-e@users.noreply.github.com>
2025-05-06 15:04:01 +1200

    Added airlines and themes
    Also set default theme to be catppuccin.

--- init.lua
+++ init.lua
@@ -33,7 +33,13 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "junegunn/goyo.vim",
+    cmd = "Goyo" -- Lazy-load only when the Goyo command is used
+  },
+  {'vim-airline/vim-airline'},
+  {'vim-airline/vim-airline-themes'}
 }
 local opts = {}
 
@@ -41,6 +47,7 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>go', '<cmd>Goyo<cr>', { desc = "Toggle Goyo" })
 
 local config = require("nvim-treesitter.configs")
 config.setup({
@@ -51,3 +58,5 @@ config.setup({
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin-latte"
+
+vim.cmd("AirlineTheme catppuccin")
