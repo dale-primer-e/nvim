f961cfb78c338de653ac184e6b273a9f0a222c4a
dale-primer-e <70494025+dale-primer-e@users.noreply.github.com>
2025-06-16 11:13:18 +1200

    Add completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,18 +9,31 @@ return {
 		"mason-org/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
+				ensure_installed = { "lua_ls", "pyright" },
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+			lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+			lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
