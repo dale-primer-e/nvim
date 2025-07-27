1ad9e42d61e4f79c1dadb7c1c34612cf1d1cf6fb
dale-primer-e <70494025+dale-primer-e@users.noreply.github.com>
2025-07-28 10:07:04 +1200

    Modify neo-tree and telescope
    Primarily to make it easier to use and navigate buffers. That's been a
    pain point for me.

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,16 +1,22 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	lazy = false,
-	opts = {
-		-- fill any relevant options here
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<cr>", { desc = "Open/close file tree on the left" })
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false,
+  opts = {
+    -- Put options here
+  },
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<cr>", { desc = "Open file tree on the left" })
+    vim.keymap.set(
+      "n",
+      "<C-b>",
+      ":Neotree buffers reveal float<cr>",
+      { desc = "Open buffers list in floating window" }
+    )
+  end,
 }
